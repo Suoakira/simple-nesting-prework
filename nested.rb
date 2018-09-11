e5d6c39a9e7b629ee1acc795d04555a822ce916d
Stephen Courtier <steves0089@gmail.com>
2018-09-11 14:49:07 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -134,6 +134,7 @@ def adding_to_dennis
         }
      }
     programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+    # used the push method but could have also
     programmer_hash
 
 end
