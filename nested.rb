c907a13f35ef46421af505ff40c4b3c8fd312784
Stephen Courtier <steves0089@gmail.com>
2018-09-11 14:48:33 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -112,7 +112,7 @@ def changing_alan
      
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info
-     programmer_hash
+     programmer_hash #return the new updated hash
      
      
 end
@@ -133,5 +133,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+    programmer_hash[:dennis_ritchie][:languages].push("Assembly")
 
 end
