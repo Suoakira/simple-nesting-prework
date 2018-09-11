17428f27b20473697131a668186410450e9087eb
Stephen Courtier <steves0089@gmail.com>
2018-09-11 14:33:53 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -39,7 +39,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-     programmer_hash[:alan_kay_is_known_for]
+     programmer_hash[:alan_kay_is_known_for][:known_for]
 end
 
 def dennis_ritchies_language
@@ -58,15 +58,17 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-    programmer_hash[:dennis_ritchie][:languages]
+    programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP, C"]
-# }
+programmer_hash = {
+  :yukihiro_matsumoto => {
+    :known_for => "Ruby",
+    :languages => ["LISP, C"]
+ }
+}
 
 	programmer_hash = 
  		{
